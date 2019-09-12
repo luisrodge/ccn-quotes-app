c2b8376f4badebe050a3a4022c76bc11f4b25b37
Luis Rodriguez <luisrodge@outlook.com>
2019-09-11 21:01:40 -0600

    Add main wrapper styled component and add global styles component

---
+++ src/components/Wrapper.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  background-color: #fff;
+  width: 800px;
+  height: 450px;
+  padding: 20px;
+  box-shadow: 1px 1px 32px 0px rgba(0, 0, 0, 0.1);
+`;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 
+
+import theme from './utils/theme';
+import Wrapper from './components/Wrapper';
 import './index.css';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #fafafa;
+    font-family: ${props => props.theme.fonts.regular};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    margin: 0;
+  }
+`
+
 const App = () => (
-  <h1>CCN Quotes</h1>
+  <ThemeProvider theme={theme}>
+    <React.Fragment>
+      <Wrapper>
+        <h1>CCN Quotes</h1>
+      </Wrapper>
+      <GlobalStyle />
+    </React.Fragment>
+  </ThemeProvider>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'));
